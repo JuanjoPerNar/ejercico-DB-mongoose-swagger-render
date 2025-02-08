d46de02a662066c581302602d1164253a16d2d68
Guanjino <jjpereiranaranjo@gmail.com>
2025-02-08 13:15:53 +0100

    challenge terminado

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const Task = require("../models/Task.js");
+const tasks = require("../docs/tasks.js");
 
 //CREATE TASK
 router.post("/create", async(req, res) => {
     try {
         const task = await Task.create({...req.body, completed: false });
-        res.status(201).send({ message: "Task successfully created", task });
+        res
+        .status(201)
+        .json({ message: "Task successfully created", task });
+
     } catch (error) {
         console.error(error);
         res
             .status(500)
-            .send({ message: "There was a problem trying to create a task" });
+            .json({ message: "There was a problem trying to create a task" });
     }
 });
 
@@ -20,9 +24,13 @@ router.post("/create", async(req, res) => {
 router.get("/", async(req, res) => {
     try {
         const tasks = await Task.find();
-        res.send(tasks);
+        res.json(tasks);
+
     } catch (error) {
         console.error(error);
+        res
+        .status(500)
+        .json({ message: 'Error getting all tasks' })
     }
 });
 
@@ -31,57 +39,89 @@ router.get("/", async(req, res) => {
 router.get("/id/:_id", async(req, res) => {
     try {
         const task = await Task.findById(req.params._id);
-        res.send(task);
+
+        if (!task) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+        res.json(task);
+
     } catch (error) {
         console.error(error);
-        res.status(500).send({
-            message: "There was a problem with the task with _id number: " +
-                req.params._id,
-        });
+        res
+        .status(500)
+        .json({ message: `There was a problem with the task with _id number: ${req.params._id}` });
     }
-}, )
+});
 
 //MARK TASK AS COMPLETED (en este endpoint no le permitimos que edite el titulo)
 
 router.put("/markAsCompleted/:_id", async(req, res) => {
         try {
             const task = await Task.findByIdAndUpdate(
-                req.params._id, {
-                    completed: true,
-                }, { new: true }
+                req.params._id, 
+                { completed: true }, 
+                { new: true }
             );
-            res.send({ message: "Task successfully updated", task });
+
+            if (!task) {
+                return res.status(404).json({ message: "Task not found"})
+            }
+
+            res
+            .json({ message: "Task successfully updated", task });
+
         } catch (error) {
             console.error(error);
-            res.status(500).send({
-                message: "There was a problem trying to update the task with _id: " +
-                    req.params._id,
-            });
+            res
+            .status(500)
+            .json({ message: `There was a problem trying to update the task with _id: ${req.params._id}` });
         }
-    }),
+    });
 
     //UPDATE TASK
 
     router.put("/id/:_id", async(req, res) => {
         try {
-            const task = await Task.findByIdAndUpdate(req.params._id, req.body, { new: true })
-            res.send({ message: "task successfully updated", task });
+            const task = await Task.findByIdAndUpdate(
+                req.params._id, 
+                {title: req.body.title}, 
+                { new: true }
+            );
+
+            if (!task) {
+                return res.status(404).json({ message: "Task not found" });
+            }
+
+            res.json({ message: "task successfully updated", task });
+
         } catch (error) {
             console.error(error);
+            res
+            .status(500)
+            .json({
+                message: `There was a problem trying to update the task with _id: ${req.params._id}`
+            })
         }
-    }),
+    });
 
     //DELETE TASK
 
     router.delete("/id/:_id", async(req, res) => {
         try {
             const task = await Task.findByIdAndDelete(req.params._id);
-            res.send({ message: "task deleted", task });
+
+            if (!task) {
+                return res.status(404).json({ message: "Task not found" });
+            }
+            res.json({ message: "task deleted", task });
+
         } catch (error) {
             console.error(error);
             res
                 .status(500)
-                .send({ message: "There was a problem trying to delete a task" });
+                .json({ message: "There was a problem trying to delete a task" });
         }
-    })
+    });
+    
 module.exports = router;
